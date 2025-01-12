e64cb433012ce7105870fbc6e43a4b0811d636a4
Armen Karakashian <magooak2003@gmail.com>
2025-01-12 12:16:58 -0500

    Update testing.py

--- Python/testing.py
+++ Python/testing.py
@@ -7,7 +7,23 @@ if len(PureDiagonals) > 0:
     AllPermsBinary.append([0]*NumOfParticles)
     AllDiagsBinary.append(PureDiagonals[0])
 
-def NSpinTriangularHeisenbergPauli(N):
+def NSpinTriangularHeisPauliString(N):
+	PauliString = []
+	
+	for i in range (N-2):
+		for j in range (2,4):
+			PauliString.append("1.0 %d X %d X"%(i+1,i+j))
+			PauliString.append("1.0 %d Y %d Y"%(i+1,i+j))
+			PauliString.append("1.0 %d Z %d Z"%(i+1,i+j))
+			
+	PauliString.append("1.0 %d X %d X"%(N-1,N))
+	PauliString.append("1.0 %d Y %d Y"%(N-1,N))
+	PauliString.append("1.0 %d Z %d Z"%(N-1,N))
+	
+	return PauliString
+
+for l in NSpinTriangularHeisPauliString(9):
+	print(l)
 
 #AllPermsBinaryNew , AllDiagsBinaryNew , transformation = apply_random_transformation([0 , 1.0 , 0] , AllPermsBinary , AllDiagsBinary , NumOfParticles)
 #AllPermsBinaryNew , AllDiagsBinaryNew = apply_single_body(AllPermsBinary, AllDiagsBinary , [1 , 2] , 'H')
